32af3f1fc5f13ba4f179c6bdde9ba50e664002ae
Tomas Hluchan <tomas.hluchan@gmail.com>
2017-12-15 15:18:07 +0100

    up

--- index.js
+++ index.js
@@ -4,10 +4,29 @@ const PORT = process.env.PORT || 5000
 
 var state = require('./read');
 
+var data = [];
+if (process.env.I_HAVE_BTC) {
+  data.push({currency:"btc",balance:parseFloat(process.env.I_HAVE_BTC)})
+}
+if (process.env.I_HAVE_LTC) {
+  data.push({currency:"ltc",balance:parseFloat(process.env.I_HAVE_LTC)})
+}
+if (process.env.I_HAVE_XRP) {
+  data.push({currency:"xrp",balance:parseFloat(process.env.I_HAVE_XRP)})
+}
+if (process.env.I_HAVE_ETH) {
+  data.push({currency:"eth",balance:parseFloat(process.env.I_HAVE_ETH)})
+}
+if (process.env.I_HAVE_BCH) {
+  data.push({currency:"bch",balance:parseFloat(process.env.I_HAVE_BCH)})
+}
+
+
+console.log(data);
 
 function index(req,res) {
   console.log("index render")
-  state().then((d)=>{
+  state(data).then((d)=>{
     console.log("state=",d);
     res.render('pages/index',d);
   }).catch(console.error);
